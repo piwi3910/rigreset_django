44db97cc7104281c53b790c63cd0facb832bb9ec
Watteel Pascal <pwatteel@pivotal.io>
2017-03-23 19:27:49 +0000

    minerlist is now dynamically filled in based on the number of miners

--- webinterface/views.py
+++ webinterface/views.py
@@ -1,6 +1,11 @@
 import django.shortcuts
+from api.models import Miners
 
 
 def index(request):
-    context = {}
+    all_miners = Miners.objects.all()
+    context = {
+        'all_miners': all_miners,
+
+                }
     return django.shortcuts.render(request, 'webinterface/index.html', context)
